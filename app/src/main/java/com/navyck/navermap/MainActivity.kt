408a7176f76581b190e29ee35e8b85ec5f6be7df
navyCK <ckck81026@gmail.com>
2021-11-21 19:38:30 +0900

    [master] MainActivity.kt

--- app/src/main/java/com/navyck/navermap/MainActivity.kt
+++ app/src/main/java/com/navyck/navermap/MainActivity.kt
@@ -2,10 +2,100 @@ package com.navyck.navermap
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager2.widget.ViewPager2
+import com.naver.maps.geometry.LatLng
+import com.naver.maps.map.CameraUpdate
+import com.naver.maps.map.MapView
+import com.naver.maps.map.NaverMap
+import com.naver.maps.map.OnMapReadyCallback
+import com.naver.maps.map.util.FusedLocationSource
+import com.naver.maps.map.widget.LocationButtonView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var naverMap: NaverMap
+    private lateinit var locationSource: FusedLocationSource
+
+    private val mapView: MapView by lazy { findViewById(R.id.mapView)}
+
+    private val currentLocationButton: LocationButtonView by lazy { findViewById(R.id.currentLocationButton) }
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mapView.getMapAsync(this)
     }
+
+    override fun onStart() {
+        super.onStart()
+        mapView.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView.onSaveInstanceState(outState)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
+    }
+
+    companion object {
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 1000
+    }
+
+    override fun onMapReady(map: NaverMap) {
+        naverMap = map
+
+        // 줌 범위 설정
+        naverMap.maxZoom = 18.0
+        naverMap.minZoom = 10.0
+
+        // 지도 위치 이동
+        val cameraUpdate = CameraUpdate.scrollTo(LatLng(37.497801, 127.027591))
+        naverMap.moveCamera(cameraUpdate)
+
+        // 현위치 버튼 기능
+        val uiSetting = naverMap.uiSettings
+        uiSetting.isLocationButtonEnabled = false // 뷰 페이져에 가려져 이후 레이아웃에 정의 하였음.
+
+        currentLocationButton.map = naverMap // 이후 정의한 현위치 버튼에 네이버맵 연결
+
+        // -> onRequestPermissionsResult // 위치 권한 요청
+        locationSource =
+                FusedLocationSource(this@MainActivity, LOCATION_PERMISSION_REQUEST_CODE)
+        naverMap.locationSource = locationSource
+
+        // 지도 다 로드 이후에 가져오기
+        getHouseListFromAPI()
+    }
+
+    private fun getHouseListFromAPI() {
+        TODO("Not yet implemented")
+    }
+
 }
